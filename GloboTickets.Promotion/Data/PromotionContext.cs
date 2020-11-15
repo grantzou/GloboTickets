bc0360f5fbf3ccaea9a108d2ce49759308b83281
Michael L Perry <michael@qedcode.com>
2020-11-15 17:35:59 -0600

    Added VenuesController from MVC template.

---
+++ GloboTickets.Promotion/Data/PromotionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GloboTickets.Promotion.Venues;
+
+namespace GloboTickets.Promotion.Data
+{
+    public class PromotionContext : DbContext
+    {
+        public PromotionContext (DbContextOptions<PromotionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GloboTickets.Promotion.Venues.Venue> Venue { get; set; }
+    }
+}
