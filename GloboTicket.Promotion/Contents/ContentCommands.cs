b5a35dc3305ef073bed915730197caf0c42a1e57
Michael L Perry <michael@qedcode.com>
2020-12-05 15:02:24 -0600

    Avoid URL unsafe base-64 characters

--- GloboTicket.Promotion/Contents/ContentCommands.cs
+++ GloboTicket.Promotion/Contents/ContentCommands.cs
@@ -20,6 +20,10 @@ namespace GloboTicket.Promotion.Contents
         {
             var sha512 = HashAlgorithm.Create(HashAlgorithmName.SHA512.Name);
             var hash = Convert.ToBase64String(sha512.ComputeHash(binary));
+            // avoid any slashes, plus signs or equal signs
+            // the following makes this base64 string url safe
+            hash = hash.Replace("/", "_");
+            hash = hash.Replace("+", "-");
 
             var exists = await repository.Content
                 .Where(c => c.Hash == hash)
